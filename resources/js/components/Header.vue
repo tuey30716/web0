5527c53ec48378f7e1b790118e47c0d473c7e926
tuey30716 <tueygo@gmail.com>
2020-12-24 11:59:54 +0700

    Initial commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app-bar
+    app
+    flat
+  >
+    <v-app-bar-nav-icon
+      class="hidden-md-and-up"
+      @click="toggleDrawer"
+    />
+
+    <v-container class="mx-auto py-0">
+      <v-row align="center">
+        <v-img
+          :src="image_src"
+          class="mr-5"
+          contain
+          height="48"
+          width="48"
+          max-width="48"
+          @click="$vuetify.goTo(0)"
+        />
+
+        <v-btn
+          v-for="(link, i) in links"
+          :key="i"
+          v-bind="link"
+          class="hidden-sm-and-down"
+          text
+          @click="onClick($event, link)"
+        >
+          {{ link.text }}
+        </v-btn>
+
+        <v-spacer />
+
+        <v-text-field
+          append-icon="mdi-magnify"
+          flat
+          hide-details
+          solo-inverted
+          style="max-width: 300px;"
+        />
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+
+  export default {
+    name: 'CoreAppBar'
+    }
+
+
+</script>
