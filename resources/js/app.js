475386b9037e8d15018d8e87ff4b3a9429b459a2
tuey30716 <tueygo@gmail.com>
2021-01-25 12:00:24 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
-
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
+Vue.component('profile-component', require('./components/ProfileComponent.vue').default);
+Vue.component('carousel-component', require('./components/Carousel.vue').default);
 
 
 const app = new Vue({
