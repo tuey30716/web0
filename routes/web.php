fccc4153f30266a84dcdc44670ed229b84377bfe
tuey30716 <tueygo@gmail.com>
2021-01-25 12:01:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@home');
+Route::get('/home', 'PageController@home');
+Route::get('/profile', 'PageController@profile');
 
-Route::get('/app', function () {
-    return view('app');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
