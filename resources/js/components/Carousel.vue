d7f3389fd862a76ef465e4c0d97c387419ea42df
tuey30716 <tueygo@gmail.com>
2021-01-25 12:00:32 +0700

    Create Carousel.vue

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+    <v-carousel v-model="model"
+      cycle
+      height="250"
+      width="100%"
+      hide-delimiter-background
+      show-arrows-on-hover>
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+      >
+        <v-sheet
+          :color="color"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">
+              Slide {{ i + 1 }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+         return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
+    }
+  },
+    }
+</script>
