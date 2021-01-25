c398165febdd54649e337cc7498d469758cf9272
tuey30716 <tueygo@gmail.com>
2021-01-25 11:19:09 +0700

    Create testlogin.blade.php

---
+++ resources/views/testlogin.blade.php
@@ -0,0 +1,11 @@
+@extends('layout\master')
+@section('content')
+
+<!-- Profile -->
+<div class="row justify-content-center mt-3">
+    <div class="col-md-8">
+        <profile-component></profile-component>
+    </div>
+</div>
+<!-- Profile End -->
+@endsection
